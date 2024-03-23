6dfcd5dbf9b1b6b63875baaf87e2c6ecd4dcb556
shruti2897 <nagmodeshruti2897@gmail.com>
2024-03-23 15:57:05 +0530

    first commit

---
+++ src/com/jenkindemo/Hello.java
@@ -0,0 +1,8 @@
+package com.jenkindemo;
+
+public class Hello {
+public static void main(String[] args) {
+	System.out.println("hello");
+	System.out.println("welcome to");
+}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author nagmo
+ *
+ */
+module jenkindemoproject {
+}
